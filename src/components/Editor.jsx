5cab17fac8efa2e7d8d3d2ec3edd2c819e2d94cf
aditya369b <aditya369b@gmail.com>
2020-10-10 22:27:42 -0700

    Fetching data from JSON

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import Line from './Lines';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Form from 'react-bootstrap/Form';
-
-let data = `across against answer awhile
-between board bottom breakfast broken build building built
-captain carried caught charge chicken circus cities clothes company
-couldn't country discover doctor doesn't dollar during
-eighth else enjoy enough everybody example except excuse
-`;
-let lines = data.split(/\n/);
+import data from './definitions';
+// let data = `across against answer awhile
+// between board bottom breakfast broken build building built
+// captain carried caught charge chicken circus cities clothes company
+// couldn't country discover doctor doesn't dollar during
+// eighth else enjoy enough everybody example except excuse
+// `;
+// let lines = data.split(/\n/);
 
 // var rows = [];
 // for (var i = 0; i < numrows; i++) {
@@ -21,8 +21,9 @@ let lines = data.split(/\n/);
 
 let rows = [];
 const line = l => {
-    for (let i=0; i<lines.length; i++){
-        rows.push(<Line data = {lines[i]}/>);
+    console.log(data);
+    for (let [key, value] of Object.entries(data)){
+        rows.push(<Line keyText={key} text = {value}/>);
     }
     return (
     <div>
@@ -35,7 +36,7 @@ const Editor = (prop) => {
 
     return (
 
-            line(lines)
+            line(data)
 
     )
 }
--- src/components/Lines.jsx
+++ src/components/Lines.jsx
@@ -7,19 +7,19 @@ import '../App.css';
 
 const Line = (props) =>{
 
-    let words = props.data.split(' ');
+    // let words = props.data.split(' ');
     const [show,setShow] = useState(false);
     const target = useRef(null);
-    console.log(words);
+    // console.log(words);
     return (
-        <div>
+        <div style={{textAlign: "left"}}>
             <span className = "Tooltip"
-       style={{color: "maroon", backgroundColor:"aqua"}}>{words[0]}
+       style={{color: "maroon", backgroundColor:"aqua"}}>{props.keyText}
               <span className="Tooltiptext">
                 Hello 
               </span> 
             </span>
-            {props.data}
+            {props.text}
             <Overlay target={target.current} show={show} placement="right">
         {({ placement, arrowProps, show: _show, popper, ...props }) => (
           <div
